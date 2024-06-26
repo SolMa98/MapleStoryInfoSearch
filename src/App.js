e14f49ef761d667737c1545d24230d2f9ba34b73
김동현 <zx12ehdgus12@gmail.com>
2024-06-26 17:18:42 +0900

    메른이 검색 결과 페이지 생성 메인 페이지 CSS 수정

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./styles/App.css";
 
 import Main from "./pages/Main.js";
+import SearchResult from "./pages/SearchResult.js";
+import Error from "./pages/Error.js";
+
 function App() {
 
   // 웹 페이지 진입 시 이동 경로 설정
@@ -10,7 +13,9 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Main />} />
+            <Route path="/" element={<Main />} />
+            <Route path="/result" element={<SearchResult />} />
+            <Route path="/*" element={<Error />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/pages/SearchResult.js
@@ -0,0 +1,14 @@
+import "../styles/SearchResult.css";
+import { useLocation } from 'react-router-dom';
+import apiCall from "../utils/apiUtils";
+const SearchResult = ( state ) => {
+    const location = useLocation();
+
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default SearchResult;
